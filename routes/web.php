a6596c4a0aa20a23fd2307dcfcb813abd2163da9
Cha <charizsalceda@gmail.com>
2021-04-03 05:04:16 +0800

    updated routes
    admin pages (show, genre, status), frontend pages update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Watchlist\IndexController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\GenreController;
+use App\Http\Controllers\Admin\ShowsController;
+use App\Http\Controllers\Admin\StatusController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Admin
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+
+// Admin Show
+Route::get('/admin/shows', [ShowsController::class, 'index'])->name('shows');
+Route::get('/admin/shows/create', [ShowsController::class, 'create'])->name('shows.create');
+
+// Admin Genre
+Route::get('/admin/genre', [GenreController::class, 'index'])->name('genre');
+Route::get('/admin/genre/create', [GenreController::class, 'create'])->name('genre.create');
+
+// Admin Status
+Route::get('/admin/status', [StatusController::class, 'index'])->name('status');
+Route::get('/admin/status/create', [StatusController::class, 'create'])->name('status.create');
+
+
+// Watchlist Landing Page
+Route::get('/', [IndexController::class, 'index'])->name('index');
