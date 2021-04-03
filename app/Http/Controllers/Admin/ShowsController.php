881db82287e46ed027da9a37b62c5675cb675e63
Cha <charizsalceda@gmail.com>
2021-04-03 20:49:43 +0800

    authorization update

--- app/Http/Controllers/Admin/ShowsController.php
+++ app/Http/Controllers/Admin/ShowsController.php
@@ -10,6 +10,12 @@ use App\Models\Genre;
 
 class ShowsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
